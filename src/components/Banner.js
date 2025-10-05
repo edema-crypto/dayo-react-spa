4c4958b9cbe6b05c2665e28bdb03c70705550e42
edema-crypto <eyimofeedemasillo@gmail.com>
2025-10-05 19:18:49 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,11 @@ import React from "react";
 function Banner() {
     return(
         <section>
-            <h1>Welcome to Prince's React SPA</h1>
+            <h1>Welcome to Dayo's React SPA</h1>
             <p>This is my banner section</p>
         </section>
     )
 }
+
 
 export default Banner;
